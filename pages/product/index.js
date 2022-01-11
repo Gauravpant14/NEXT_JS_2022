1b0529deec93f309b2aac0c95486da625fb88261
gauravpant14 <aadeezcity@gmail.com>
2022-01-11 15:16:06 +0530

    navigation to different Route using link

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,13 +1,11 @@
-import React from 'react'
-
+import Link from "next/link"  
 const ProductList = () => {
     return (
         <div>
-            <h1>Product 1</h1>
-            <h1>Product 2</h1>
-            <h1>Product 3</h1>
-
-
+            <Link href="/"><a>Home</a></Link>
+            <h1><Link href="/product/1">Product 1</Link></h1>
+            <h1><Link href="/product/2">Product 2</Link></h1>
+            <h1><Link href="/product/3" replace>Product 3</Link></h1>
         </div>
     )
 }
