320de4cc7d1c7d558f3a00155ab3ecb867d50ae7
gauravpant14 <aadeezcity@gmail.com>
2022-01-11 12:59:38 +0530

    static page in dynamic routing

---
+++ pages/product/laptop.js
@@ -0,0 +1,12 @@
+
+
+const Laptop = () => {
+    return (
+        <div>
+           <h2>when we give laptop in url (e.g. product/laptop)</h2> 
+           <h3>Initially Next will check if the laptop file is availabe inside the folder, if is avaliable then it'll not go into dynamic finind, it will pick that static file</h3>
+        </div>
+    )
+}
+
+export default Laptop
